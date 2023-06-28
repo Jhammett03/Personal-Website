6491509cc3cd76532d75364c7503752b174217eb
Jared Hammett <jaredhammett452@gmail.com>
2023-06-28 14:06:04 -0700

    myProjects

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 function aboutMe() {
     alert("I am a student at Cal Poly San Luis Obispo");
 }
+
+function myProjects() {
+    alert("I am currently working on a project that will allow users to create a profile and find other users with similar interests");
+}
